7e10daff7bd75ddd73cece0758530e2dcf901fdb
Kaene Karabo Lebakeng <kaenekarabolebakeng@gmail.com>
2021-07-14 11:53:17 +0200

    Succesfully implemeted drag and drop.

--- assets/js/iem/drag_and_drop.js
+++ assets/js/iem/drag_and_drop.js
@@ -1,3 +1,5 @@
+//import {getFeature} from 'features.js';
+
 function allowDrop(ev) {
     ev.preventDefault();
 }
@@ -8,6 +10,38 @@ function drag(ev) {
   
 function drop(ev) {
     ev.preventDefault();
-    var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
+    var featureId = ev.dataTransfer.getData("text");
+    console.log("feature id",featureId);
+    console.log("quad id",getQuadIdNumber(ev.target.id));
+    ev.target.appendChild(document.getElementById(featureId));
+
+    db.collection("Feature").doc(featureId).update({
+        quadId : getQuadIdNumber(ev.target.id)
+    }).then(()=>{
+        console.log("Quad updated successfully!");
+    }).catch((error)=>{
+        console.error("Error updating quad ", error);
+    });
+}
+
+function getQuadIdNumber(quadId) {
+    switch (quadId) {
+        case "featuresList":
+            return 0;
+            
+        case "quad1":
+            return 1;
+            
+        case "quad2":
+            return 2;
+            
+        case "quad3":
+            return 3;
+            
+        case "quad4":
+            return 4;
+               
+        default:
+            return -1;
+    }
 }
